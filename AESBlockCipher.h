21774f030317971768f7df15a3b09c01465ce6e6
Howard Yeh <howard@metacircus.com>
2017-10-14 10:39:07 +0800

    refactor s01c07

---
+++ AESBlockCipher.h
@@ -0,0 +1,34 @@
+//
+// Created by Howard Yeh on 2017-10-14.
+//
+#pragma once
+#include "openssl/aes.h"
+
+class AESBlockCipher {
+public:
+    std::string key;
+
+    AESBlockCipher(std::string key) {
+        this->key = key;
+        this->buf.resize(16);
+
+        AES_set_decrypt_key((const unsigned char *) key.c_str(), 16 * 8, &this->decryptKey);
+        AES_set_encrypt_key((const unsigned char *) key.c_str(), 16 * 8, &this->encryptKey);
+    }
+
+    std::string decryptBlock(std::string::iterator it) {
+        AES_ecb_encrypt((const unsigned char *) it.base(), (unsigned char *) buf.c_str(), &this->decryptKey, AES_DECRYPT);
+        return buf;
+    }
+
+    std::string encryptBlock(std::string::iterator it) {
+        AES_ecb_encrypt((const unsigned char *) it.base(), (unsigned char *) buf.c_str(), &this->encryptKey, AES_ENCRYPT);
+        return buf;
+    }
+private:
+
+    AES_KEY decryptKey;
+    AES_KEY encryptKey;
+
+    std::string buf;
+};
--- s01c07.cpp
+++ s01c07.cpp
@@ -1,31 +1,24 @@
-//#include <openssl/conf.h>
 #include <iostream>
 #include <fstream>
 
-#include "openssl/aes.h"
-
 #import "utils.cpp"
 
-int main() {
-    auto key = AES_KEY();
+#include "AESBlockCipher.h"
 
-    AES_set_decrypt_key((const unsigned char *) "YELLOW SUBMARINE", 16 * 8, &key);
+int main() {
+
+    AESBlockCipher cipher("YELLOW SUBMARINE");
 
     auto data = readBase64File("./7.txt");
 
     std::string out;
-    std::string outbuf;
-    outbuf.resize(16);
 
     for (int i = 0; i < data.length(); i += 16) {
         auto it = data.begin() + i;
-        AES_ecb_encrypt((const unsigned char *) it.base(), (unsigned char *) outbuf.c_str(), &key, AES_DECRYPT);
-        out += outbuf;
+        auto decryptedBlock = cipher.decryptBlock(it);
+        out += decryptedBlock;
     }
 
-
-//    std::cout << "data:" << data << std::endl;
     std::cout << out << std::endl;
     std::cout << "ok" << std::endl;
-
 }
